541d2b0c55b89de035156975327623a94da7a006
Juli Ramon <juli@nexumlabs.com>
2022-08-17 22:17:49 +0200

    chore: front --> initial commit - landing

---
+++ components/global/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="pt-12 pb-6 relative z-10">
+      <div className="container">
+        <div className="w-full flex flex-wrap items-center justify-between">
+          <span className="uppercae text-primary-500 text-sm font-fonseca">
+            01.04.2023
+          </span>
+          <span className="uppercase text-primary-500 text-sm font-fonseca">
+            #lagranescapada2023
+          </span>
+          <span className="font-tenez uppercase text-primary-500 text-md text-right leading-tight">
+            Andrea
+            <br /> & Juli
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+const colors = {
+  primary: {
+    100: "#ffffff",
+    200: "#DAEAEF",
+    300: "#E7EBF5",
+    400: "#7BA5D0",
+    500: "#2F6FAF",
+  },
+};
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "false", // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: "1.5rem",
+    },
+    extend: {
+      colors: {
+        ...colors,
+      },
+      fontSize: {
+        base: "17px",
+      },
+      fontFamily: {
+        tenez: ["Tenez", "sans-serif"],
+        fonseca: ["Fonseca", "sans-serif"],
+      },
+      borderRadius: {
+        350: "350px",
+      },
+      lineHeight: {
+        1.1: "1.1",
+        12: "3.2",
+      },
+      minWidth: {
+        "1/2": "50%",
+      },
+      height: {
+        "1/2": "50%",
+        "50vh": "50vh",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+};
